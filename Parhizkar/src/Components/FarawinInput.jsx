1d94292c9b8f7c00bd88647b03d276fef63e7cea
Mmdrzphzkr <134147947+Mmdrzphzkr@users.noreply.github.com>
2023-09-16 15:18:50 +0330

    Commenting

--- Parhizkar/src/Components/FarawinInput.jsx
+++ Parhizkar/src/Components/FarawinInput.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import '../App.css'
 
+// Main func with props passed
 export default function FarawinInput (props){
-    
-    const handleOnBlur = (event)=>{
+    // An arrow func for getting input Values which is working with onchange event
+    const handleOnChange = (event)=>{
         const text = event.target.value;
         props.input(text)
     }
     return (
         <div>
-            <input className='p-2 border-slate-800 border-2 rounded-md outline-none' onChange={handleOnBlur}  type="text"  />
+            <input className='p-2 border-slate-800 border-2 rounded-md outline-none' onChange={handleOnChange}  type="text"  />
         </div>
     )
 }
